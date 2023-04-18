83d3f6016a1ae9269886f9eefe94b543b2c66641
Jasneet <jasneetpalsingh619@gmail.com>
2023-04-17 22:59:56 -0600

    added bank dataset and css

--- banks/src/app/banks/banks.component.ts
+++ banks/src/app/banks/banks.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Bank } from '../bank';
+import { BANKS } from '../banks';
+
 
 @Component({
   selector: 'app-banks',
   templateUrl: './banks.component.html',
   styleUrls: ['./banks.component.css']
 })
-export class BanksComponent {
 
+export class BanksComponent implements OnInit {
+  banks = BANKS;
+  selectedBank!: Bank;
+
+  constructor() { }
+
+  onSelect(bank: Bank): void {
+    this.selectedBank = bank;
+  }
+
+  ngOnInit():  void{
+  }
 }
