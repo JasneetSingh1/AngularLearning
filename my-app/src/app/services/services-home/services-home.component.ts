50003de2f46e1b572bd49f774bee42e063565146
Jasneet <jasneetpalsingh619@gmail.com>
2023-04-30 18:52:11 -0600

    added structural and attribute directives to service component

--- my-app/src/app/services/services-home/services-home.component.ts
+++ my-app/src/app/services/services-home/services-home.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesHomeComponent implements OnInit {
 
+  bankingServices: any[] = [
+    {
+      "type": 'Issue business loans',
+      "years": 2,
+      "category": "loans"
+    },
+    {
+      "type": 'Provide periodic reports and analysis',
+      "years": 7,
+      "category": "reports"
+    },
+    {
+      "type": 'Open new checking and/or saving accounts',
+      "years": 5,
+      "category": "financial_accounts"
+    },
+    {
+      "type": 'Guide clients through their savings, loan, mortgage, and retirement options',
+      "years": 6,
+      "category":"options"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
