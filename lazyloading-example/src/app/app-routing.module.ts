08848110c9979c67fe613ed423a5f5626a5fbde5
Jasneet <jasneetpalsingh619@gmail.com>
2023-04-30 21:29:34 -0600

    added code for lazy loading in the routing-module

--- lazyloading-example/src/app/app-routing.module.ts
+++ lazyloading-example/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'stocks', 
+    loadChildren: () => import('./stocks/stocks.module').then(m => m.StocksModule)
+  }, 
+  { path: 'bonds',
+   loadChildren: () => import('./bonds/bonds.module').then(m => m.BondsModule) 
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
