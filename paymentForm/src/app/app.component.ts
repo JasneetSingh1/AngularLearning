a0d9a7bbff3fa6a6ef7fd516f5d8ccd070c5e820
Jasneet <jasneetpalsingh619@gmail.com>
2023-04-30 23:23:24 -0600

    worked with pipes

---
+++ paymentForm/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  
+})
+export class AppComponent {
+  title = 'Payment Form';
+
+  
+  name!: string;
+  date!: string;
+  amount!: number;
+  interest!: number;
+  total!: number;
+
+
+  
+
+  
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  onInterestChange(value: string) {
+    this.interest = parseFloat(value);
+  }
+
+
+}
