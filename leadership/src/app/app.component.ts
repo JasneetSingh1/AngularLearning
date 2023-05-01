6fb9b0705ad7570152091ac67a3dabdef347334d
Jasneet <jasneetpalsingh619@gmail.com>
2023-04-30 20:18:44 -0600

    set up the data set, DI to return data when it is subscribed to the observable

--- leadership/src/app/app.component.ts
+++ leadership/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { PeopleService } from 'src/shared/people.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'leadership';
+  peopleArray: any;
+  
+  constructor(private peopleService: PeopleService) { }
+
+  getPeople(){
+    this.peopleService.getPeople().subscribe(people => {
+        this.peopleArray = people
+    });
+  }
+
+  ngOnInit() {
+    this.getPeople();
+  }
 }
